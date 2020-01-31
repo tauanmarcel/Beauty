6c90ec133c90da2bb98dbdb7697e78afb3e6a465
Tauan Marcel <tauanmarcel@hotmail.com>
2020-01-30 22:05:47 -0300

    Autenticação e recuperação de senha criados

--- backend/database/migrations/1503250034279_user.js
+++ backend/database/migrations/1503250034279_user.js
@@ -7,9 +7,12 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('avatar')
+      table.string('token')
+      table.timestamp('token_created_at')
       table.timestamps()
     })
   }
